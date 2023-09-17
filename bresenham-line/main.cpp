6f257b05cc27be66e5e3314149623bf815e83220
AdiAkhileshSingh15 <adiakhilesh.singh.cse21@itbhu.ac.in>
2023-09-17 17:15:53 +0530

    Fixes to bres-line

--- bresenham-line/main.cpp
+++ bresenham-line/main.cpp
@@ -5,45 +5,61 @@
 
 int xa, ya, xb, yb;
 
-void plotPoint(int x, int y) {
+void plotPoint(int x, int y)
+{
     glBegin(GL_POINTS);
     glVertex2i(x, y);
     glEnd();
 }
 
-void display() {
+void display()
+{
     int x, y, dx, dy, p;
-    dx = abs(xb-xa);
-    dy = abs(yb-ya);
-    x = xa, y = ya;
-    p = 2*dy - dx;
-    
+    dx = abs(xb - xa);
+    dy = abs(yb - ya);
+    p = 2 * dy - dx;
+    if (xa > xb)
+    {
+        x = xb;
+        y = yb;
+        xb = xa;
+    }
+    else
+    {
+        x = xa;
+        y = ya;
+    }
+
     plotPoint(x, y);
 
-    for (int k = 0; k < dx; k++) {
-        if (p < 0) {
-            plotPoint(x+1, y);
-            x = x+1;
-            p = p + 2*dy;
+    for (int k = 0; k < dx; k++)
+    {
+        if (p < 0)
+        {
+            x = x + 1;
+            p = p + 2 * dy;
         }
-        else {
-            plotPoint(x+1, y+1);
-            x = x+1, y = y+1;
-            p = p + 2*dy - 2*dx;
+        else
+        {
+            x = x + 1, y = y + dy / abs(dy);
+            p = p + 2 * dy - 2 * dx;
         }
+        plotPoint(x, y);
     }
 
     glFlush();
 }
 
-void myInit(void) {
+void myInit(void)
+{
     glClear(GL_COLOR_BUFFER_BIT);
-	glClearColor(0.0, 0.0, 0.0, 1.0);
-	glMatrixMode(GL_PROJECTION);
-	gluOrtho2D(0, 500, 0, 500);
+    glClearColor(0.0, 0.0, 0.0, 1.0);
+    glMatrixMode(GL_PROJECTION);
+    gluOrtho2D(0, 500, 0, 500);
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
 
     std::cout << "Value of xa : ";
     std::cin >> xa;
@@ -55,13 +71,13 @@ int main(int argc, char **argv) {
     std::cin >> yb;
 
     glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);
-	glutInitWindowSize(500, 500);
-	glutInitWindowPosition(0, 0);
-	glutCreateWindow("Bresenham's Line Drawing: ");
-	myInit();
-	glutDisplayFunc(display);
-	glutMainLoop();
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize(500, 500);
+    glutInitWindowPosition(0, 0);
+    glutCreateWindow("Bresenham's Line Drawing: ");
+    myInit();
+    glutDisplayFunc(display);
+    glutMainLoop();
 
     return 0;
 }
